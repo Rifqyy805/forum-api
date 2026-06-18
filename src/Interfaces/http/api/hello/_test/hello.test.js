1d744ccfdf8161138c7b64a92ebd1c6aa7b7ec3a
Rifqy Ardian <rifqyardian853@gmail.com>
2026-06-18 07:03:54 +0700

    fix: fix hello world test

--- src/Interfaces/http/api/hello/_test/hello.test.js
+++ src/Interfaces/http/api/hello/_test/hello.test.js
@@ -1,5 +1,5 @@
 describe('Hello World', () => {
-  it('should fail intentionally for CI demo', () => {
-    expect(true).toBe(false);
+  it('should pass', () => {
+    expect(true).toBe(true);
   });
 });
